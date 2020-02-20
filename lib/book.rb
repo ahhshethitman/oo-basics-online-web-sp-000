d6eaf239fa550ffcbc7a7a4b530a5738744171f8
Hunter Sides <hunter.s.1337@gmail.com>
2020-02-20 23:04:55 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,47 +1,17 @@
 class Book
-  
+  attr_accessor :author, :page_count, :genre
+ 
   def initialize(title)
-    @title = title 
-  end 
-  
-  #properties
-  
-  
+    @title = title
+  end
+ 
   def title
     @title
   end
-  
-  def author=(author)
-    @author
-  end 
-  
-  def author
-    @author
-  end
-  
-  def page_count=(num)
-    @page_count = num 
-  end 
-  
-  def page_count
-    @page_count
-  end
-  
-  def genre=(genre)
-    @genre
-  end 
-  
-  def genre
-    @genre
-  end 
-  
-  #actions 
-  
-  def turn_page 
+ 
+  def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-  
-end 
-
-Book.new("And Then There Were None")
+ 
+end
 
